bc0e7a14adb0b0521c57562b1e8134432ed06d4b
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-20 01:48:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++
@@ -1,9 +0,0 @@
-def square_array(numbers)
-  numbers = [1, 2, 3]
-  numbers.each do |number|
-    puts math.sqrt(#{numbers})
-end
-  new_numbers = [9, 10, 16, 25]
-  new_numbers.each do |new_number|
-    puts math.sqrt(#{new_numbers})
-  end
